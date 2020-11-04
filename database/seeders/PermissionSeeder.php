e068e090d9dbcca25821b230704a947112eb1875
hanyun <1355081829@qq.com>
2020-11-04 17:27:07 +0800

    修改权限，添加页面内权限限制

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -43,25 +43,37 @@ class PermissionSeeder extends Seeder
                         'path' => '/role/list',
                         'parent_id' => 0,
                         'is_menu' => 1,
+                        'children' => [
+                            [
+                                'api_path' => '/admin/role/list',
+                                'rule' => '/admin/role/list',
+                                'method' => 'get',
+                                'title' => '查看角色列表',
+                                'path' => 'roleList',
+                                'parent_id' => 0,
+                                'is_menu' => 1,
+                            ],
+                            [
+                                'api_path' => '/admin/role/{roleId}',
+                                'rule' => '/admin/role/*',
+                                'method' => 'delete',
+                                'title' => '删除角色',
+                                'path' => 'deleteRole',
+                                'parent_id' => 0,
+                                'is_menu' => 0,
+                            ],
+                            [
+                                'api_path' => '/admin/role/{roleId}',
+                                'rule' => '/admin/role/*',
+                                'method' => 'post',
+                                'title' => '编辑角色',
+                                'path' => 'editRole',
+                                'parent_id' => 0,
+                                'is_menu' => 0,
+                            ]
+                        ]
                     ],
-                    [
-                        'api_path' => '/admin/role/{roleId}',
-                        'rule' => '/admin/role/*',
-                        'method' => 'delete',
-                        'title' => '删除角色',
-                        'path' => 'deleteRole',
-                        'parent_id' => 0,
-                        'is_menu' => 1,
-                    ],
-                    [
-                        'api_path' => '/admin/role/{roleId}',
-                        'rule' => '/admin/role/*',
-                        'method' => 'post',
-                        'title' => '编辑角色',
-                        'path' => 'editRole',
-                        'parent_id' => 0,
-                        'is_menu' => 1,
-                    ]
+
                 ]
             ],
             [
@@ -81,25 +93,37 @@ class PermissionSeeder extends Seeder
                         'path' => '/admin/list',
                         'parent_id' => 0,
                         'is_menu' => 1,
+                        'children' => [
+                            [
+                                'api_path' => '/admin/admin/list',
+                                'rule' => '/admin/admin/list',
+                                'method' => 'get',
+                                'title' => '查看管理员列表',
+                                'path' => 'adminList',
+                                'parent_id' => 0,
+                                'is_menu' => 0,
+                            ],
+                            [
+                                'api_path' => '/admin/{adminId}',
+                                'rule' => '/admin/admin/*',
+                                'method' => 'post',
+                                'title' => '编辑管理员信息',
+                                'path' => 'editAdmin',
+                                'parent_id' => 0,
+                                'is_menu' => 0,
+                            ],
+                            [
+                                'api_path' => '/admin/{adminId}',
+                                'rule' => '/admin/admin/*',
+                                'method' => 'delete',
+                                'title' => '删除管理员',
+                                'path' => 'deleteAdmin',
+                                'parent_id' => 0,
+                                'is_menu' => 0,
+                            ]
+                        ]
                     ],
-                    [
-                        'api_path' => '/admin/{adminId}',
-                        'rule' => '/admin/admin/*',
-                        'method' => 'post',
-                        'title' => '编辑管理员信息',
-                        'path' => 'editAdmin',
-                        'parent_id' => 0,
-                        'is_menu' => 0,
-                    ],
-                    [
-                        'api_path' => '/admin/{adminId}',
-                        'rule' => '/admin/admin/*',
-                        'method' => 'delete',
-                        'title' => '删除管理员',
-                        'path' => 'deleteAdmin',
-                        'parent_id' => 0,
-                        'is_menu' => 0,
-                    ]
+
                 ]
             ]
 
@@ -115,7 +139,15 @@ class PermissionSeeder extends Seeder
                 unset($children['children']);
                 foreach ($children as $val) {
                     $val['parent_id'] = $result->id;
-                    Permission::create($val);
+                    @$children = $val['children'];
+                    unset($val['children']);
+                    $res = Permission::create($val);
+                    if ($children) {
+                        foreach ($children as $c) {
+                            $c['parent_id'] = $res->id;
+                            Permission::create($c);
+                        }
+                    }
                 }
             }
         }
