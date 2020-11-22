06ce13a9a8997eaecfe6a3df167ad90445064d6c
hanyun <1355081829@qq.com>
2020-11-22 15:53:12 +0800

    订单列表 地址

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Order;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class OrderFactory extends Factory
@@ -21,9 +22,10 @@ class OrderFactory extends Factory
      */
     public function definition()
     {
+        $userList = User::select('id')->get();
         return [
             //
-            'user_id' => $this->faker->numberBetween(1, 100),
+            'user_id' => $this->faker->randomElement($userList->pluck('id')),
             'pay_status' => $this->faker->numberBetween(0, 6),
             'order_number' => $this->faker->date('Ymd') . $this->faker->time('His'),
             'trade_price' => $this->faker->randomFloat(2, 0, 1000),
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,6 +25,7 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name,
+            'mobile' => $this->faker->phoneNumber,
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
             'password' => Hash::make('123456abc'),
