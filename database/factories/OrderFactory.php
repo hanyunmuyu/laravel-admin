6941f1dccc4a3e90e7a57fe52d6ff368d4afa17c
hanyun <1355081829@qq.com>
2020-11-22 19:59:09 +0800

    订单列表 搜索

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -26,7 +26,7 @@ class OrderFactory extends Factory
         return [
             //
             'user_id' => $this->faker->randomElement($userList->pluck('id')),
-            'pay_status' => $this->faker->numberBetween(0, 6),
+            'pay_status' => $this->faker->numberBetween(0, 7),
             'order_number' => $this->faker->date('Ymd') . $this->faker->time('His'),
             'trade_price' => $this->faker->randomFloat(2, 0, 1000),
             'original_price' => $this->faker->randomFloat(2, 0, 1000),
